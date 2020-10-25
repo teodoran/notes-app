ba6d124f70835ed1c5eaeafe60ddcc3e524d7d25
Alexander Hoem Rosbach <ahr@computas.com>
2020-10-25 17:59:06 +0100

    Add k8s manifests

---
+++ notat-api/src/main/kotlin/com/computas/devops101/notatapi/HealthController.kt
@@ -0,0 +1,14 @@
+package com.computas.devops101.notatapi
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/healthz")
+class HealthController {
+
+    @GetMapping
+    fun getHealthz(): String = "Healthy"
+
+}
