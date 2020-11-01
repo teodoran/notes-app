fabc40d3da046e6dd0f9ebdf6464a6ec180ed979
Alexander Hoem Rosbach <ahr@computas.com>
2020-11-01 15:45:52 +0100

    Add metrics for prometheus

--- notat-api/build.gradle.kts
+++ notat-api/build.gradle.kts
@@ -17,9 +17,11 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.micrometer", "micrometer-registry-prometheus" , "1.6.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
