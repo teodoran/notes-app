d2af400a081bfe99a98edff207ad74b26b537e6c
Teodor Elstad <teodor@andeelstad.com>
2020-11-01 21:56:37 +0100

    HealthController should not be healthy initially
    As far as I can tell, we would like the HealthController to be "Not Healthy", since the build should fail for the workshop participants initially. Not really sure if we would like to have this only on the main-branch, or if there's a more suitable branch to put this on :thinking:

--- notat-api/src/main/kotlin/com/computas/devops101/notatapi/HealthController.kt
+++ notat-api/src/main/kotlin/com/computas/devops101/notatapi/HealthController.kt
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController
 class HealthController {
 
     @GetMapping
-    fun getHealthz(): String = "Healthy"
+    fun getHealthz(): String = "Not Healthy"
 
 }
